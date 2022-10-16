223059d0a61b8f896bf258604af2f661396ac41b
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-16 19:50:49 +0530

    Ability to create a contact from console

--- AdressBookSystem/AdressBookSystem/Program.cs
+++ AdressBookSystem/AdressBookSystem/Program.cs
@@ -6,10 +6,31 @@
         {
 
             AdressBook addressBook = new AdressBook();
-            addressBook.addContacts("Kavya", "Bs", "Mysore", "Karnataka", "India", 571114, 7090324227, "abcd@gmail.com");
+            takeInputAndAddToContacts(addressBook);
+            takeInputAndAddToContacts(addressBook);
             addressBook.print();
             Console.ReadLine();
-            
+        }
+        public static void takeInputAndAddToContacts(AdressBook addressBook)
+        {
+            Console.WriteLine("Enter FirstName");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber");
+            long phoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email id");
+            string email = Console.ReadLine();
+            addressBook.addContacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            Console.ReadLine();
         }
     }
 }
